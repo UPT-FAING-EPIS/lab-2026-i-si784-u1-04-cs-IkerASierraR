5cedd90596b9ac8ec5a97fa4e170078523ee33e2
Iker Sierra <is2023077090@virtual.upt.pe>
2026-06-24 23:44:49 -0500

    Fix Lab 4 tests: rename src to Shorten, add dummy test, copy ci-cd.yml to .githu

---
+++ Shorten/DummyTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shorten.Tests
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
